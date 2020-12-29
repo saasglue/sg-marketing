61513b45231ac7bab5673bd8c8680e47344d06eb
Bart Wood <bartwood@gmail.com>
2020-12-28 21:04:40 -0700

    mobile spacing hell

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,504 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div v-cloak class="popup" :hidden="hideBurgerPopup">
+      <div class="popup-content" style="width: 240px;">
+        <div class="popup-close">
+          <div class="popup-close-x">
+            <a href="" style="color: black; font-size: 24px;" @click.prevent="hideBurgerPopup = !hideBurgerPopup">
+              x
+            </a>
+          </div>
+        </div>
+        <div class="burger-popup-links">
+          <div class="burger-popup-links-left-aligned">            
+            <router-link to="/" class="burger-popup-link" @click.native="hideBurgerPopup=true">Home</router-link>            
+            <router-link to="/how" class="burger-popup-link" @click.native="hideBurgerPopup=true">How it works</router-link>
+            <router-link to="/why" class="burger-popup-link" @click.native="hideBurgerPopup=true">Why saas glue?</router-link>
+            <router-link to="/pricing" class="burger-popup-link" @click.native="hideBurgerPopup=true">Pricing</router-link>
+            <router-link to="/resources" class="burger-popup-link" @click.native="hideBurgerPopup=true">Resources</router-link>
+            <span class="burger-popup-spacer"></span>
+            <a href="https://console.saasglue.com" class="burger-popup-link round-button">
+              Login
+              <span style="font-size: 16px; font-weight: 300;">
+                >
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <div class="main-top-container">
+      <div class="main-top" style="background-image: linear-gradient(90deg, black, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.15),  rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.25), black), url(background1.jpg);">
+        <div class="main-nav-bar">
+          
+          <div class="main-nav-bar-left">
+            <img src="MainLogo.png">
+          </div>
+          
+          <div class="main-nav-bar-right">
+            <div class="big-menu">
+              
+              <span v-if="isRouterLinkActive('/')" class="menu-selected">.</span>
+              <router-link to="/" class="main-nav-link">Home</router-link>
+
+              <span v-if="isRouterLinkActive('/how')" class="menu-selected">.</span>
+              <router-link to="/how" class="main-nav-link">How it works</router-link>
+
+              <span v-if="isRouterLinkActive('/why')" class="menu-selected">.</span>
+              <router-link to="/why" class="main-nav-link">Why saas glue?</router-link>
+
+              <span v-if="isRouterLinkActive('/pricing')" class="menu-selected">.</span>
+              <router-link to="/pricing" class="main-nav-link">Pricing</router-link>
+
+              <span v-if="isRouterLinkActive('/resources')" class="menu-selected">.</span>
+              <router-link to="/resources" class="main-nav-link">Resources</router-link>
+
+              <span class="main-nav-spacer"></span>
+              <a href="https://console.saasglue.com" class="main-nav-link round-button">
+                Login
+                <span style="font-size: 16px; font-weight: 300;">
+                  >
+                </span>
+              </a>
+            </div>
+            <div class="burger-menu">
+              <a href="" class="main-nav-link"  @click.prevent="hideBurgerPopup = !hideBurgerPopup"><img src="HamburgerLines.png" width="30px;"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="splash-row">
+          <div class="splash-colunn">
+            <div class="splash-message" v-html="splashMessage"></div>
+            <br>          
+            <a href="https://console.saasglue.com" class="try-it-button">
+              Try it free 
+              <span style="margin-left: 5px; font-size: 16px; font-weight: 300;">
+                >
+              </span>
+            </a>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
     <router-view/>
+    
+    <div class="footer">
+      <div class="footer-left">
+        &#169; 2020, saas glue LLC​
+      </div>
+      <div class="footer-right">
+        Contact us
+      </div>
+    </div>
   </div>
 </template>
 
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  private hideBurgerPopup = true;
+
+  private isRouterLinkActive(path: string): boolean {
+    return this.$router.currentRoute.path === path;
+  }
+
+  private splashMessage = 'Any Script<br>Any Device<br>Anywhere';
+
+  @Watch('$route', {immediate: true, deep: true})
+  private onRouteChanged() {
+    switch(this.$router.currentRoute.path){
+      case '/how':
+        this.splashMessage = `Any Script<br>Any Device<br>Anywhere`;
+        break;
+      default:
+        this.splashMessage = `Integrate Anything<br>Automate Everything`;
+    }
+  }
+
+}
+</script>
+
+
 <style>
+[v-cloak] {display: none}
+
+
+body {
+  font-family: 'Nunito Sans', 'Avenir';
+}
+
+html, body {
+  padding: 0;
+  margin: 0;
+}
+
+.header {
+  margin: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@media (max-width: 1230px) {
+  .big-menu {
+    display: none;
+  }
+  .burger-menu {
+    display: block;
+  }
+  .main-top {
+    height: 500px;
+  }
+  .blurb-spacer {
+    width: 10px;
+  }
+  .blurb-item {
+    font-size: calc(12px + 1vw);
+  }
+  .promo-video {
+    
+  }
+  .blurb-outer-container {
+    
+  }
+  .blurb-container {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  .blurb-item {
+    margin-bottom: 15px;
+  }
+  .spaced-skew {
+
+  }
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+
+@media (min-width: 1230px) {
+  .big-menu {
+    display: block;
+  }
+  .burger-menu {
+    display: none;
+  }
+  .main-top {
+    height: 600px;
+  }
+  .blurb-spacer {
+    width: 400px;
+  }
+  .blurb-item {
+    font-size: 22px;
+    margin-left: 20px;
+    margin-right: 20px
+  }
+  .promo-video {
+    width: 656px;
+    height: 369px;
+  }
+  .blurb-outer-container {
+    
+  }
+  .blurb-container {
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-left: 40px;
+    margin-right: 40px;
+  }
+  .spaced-skew {
+    margin-top: calc(60px + 4vw);
+  }
 }
+
+.popup {
+  position: fixed; 
+  z-index: 1; 
+  left: 0;
+  top: 0;
+  width: 100%; 
+  height: 100%; 
+  overflow: auto; 
+  background-color: rgba(0,0,0,0.5);
+}
+
+.hidden {
+  display: none;
+}
+
+.popup-close {
+  position: relative;
+  margin-bottom: 20px;
+}
+
+.popup-close-x {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+}
+
+.popup-content {
+  background-color: #fefefe;
+  margin: 15% auto; 
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; 
+  border-radius: 10px;
+}
+
+.burger-popup-links {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 50px;
+  margin-top: 25px;
+  margin-bottom: 50px;
+}
+
+.burger-popup-links-left-aligned {
+  display: flex;
+  flex-direction: column;
+  align-self: flex-start;
+}
+
+.burger-popup-link {
+  color: black;
+  margin-top: 15px;
+  font-size: 20px;
+}
+
+.main-top-container {
+  display: flex;
+  justify-content: center;
+  background-image: linear-gradient(90deg, black, black);
+  width: 100%;
+  margin-left: 0px;
+  margin-right: 0px; 
+  padding-left: 0px;
+  padding-right: 0px;
+}
+
+.splash-row {
+  display: flex;
+  justify-content: center;
+  width: 100%;  
+}
+
+.splash-column {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+}
+
+.splash-message {
+  font-size: calc(20px + 3vw);
+  letter-spacing: 3px;
+  margin-top: 100px;
+  font-weight: 700;    
+  color: white;
+  text-shadow: 0px 2px 1px rgb(150, 148, 148), 0px -2px 1px rgb(150, 148, 148), 2px 0px 1px rgb(150, 148, 148), -2px 0px 1px rgb(150, 148, 148);  
+}
+
+.try-it-button {
+  font-size: calc(14px + 1vw);
+  letter-spacing: 2px;
+  font-weight: 700;
+  color: white;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 14px;
+  padding-right: 14px;
+  margin-top: 30px;  
+  background-color: rgba(32, 31, 31, 0.5);
+  border-radius: 12px;  
+}
+
+.main-top {  
+  flex-grow: 1;
+  flex-basis: max-content;
+  max-width: 1940px;
+}
+
+.main-nav-bar {
+  padding-left: 30px; 
+  padding-right: 30px;
+  padding-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.main-nav-bar-left {
+  display: flex;
+  align-items: flex-start;
+}
+
+.main-nav-bar-right {
+  display: flex;
+  align-items: center;
+  color: white;
+  font-weight: 700;
+  font-size: 20px;
+}
+
+.main-nav-link {
+  margin-left: 12px;
+  margin-right: 12px;
+  letter-spacing: 1.5px;
+}
+
+.round-button {
+  border-radius: 12px;
+  padding-left: 14px;
+  padding-right: 14px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  background-color: rgba(200, 202, 202, 0.25);
+}
+
+.menu-selected {
+  position: relative; 
+  bottom: 6px; 
+  margin-left: 8px; 
+  font-size: 28px;
+}
+
+.main-nav-spacer {
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+
+.skew-background {
+  background-image: linear-gradient(135deg, black, rgb(53, 119, 241), rgb(53, 119, 241), rgb(118, 183, 209), rgb(53, 119, 241));
+  display: flex;
+  justify-content: center;
+  padding-top: calc(12px + 4vw);
+  color: white;
+  font-weight: 700;
+  font-size: calc(12px + 1vw);
+  letter-spacing: 2px;  
+  margin-left: 0px;
+  margin-right: 0px;
+}
+
+.skew-background-up {
+  transform-origin: top left;
+  transform: skewY(-1.75deg);  
+}
+
+.skew-content-up {
+  transform: skewY(1.75deg);    
+}
+
+.skew-background-down {
+  transform-origin: top left;
+  transform: skewY(1.75deg);
+}
+
+.skew-content-down {
+  transform: skewY(-1.75deg);
+  letter-spacing: 2px;
+  line-height: 1.8em;
+  letter-spacing: 3px;
+}
+
+.blurb-outer-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  letter-spacing: 2px;
+  line-height: 1.8em;
+  font-weight: 700;
+  font-size: 20px;
+  color: #423F3F;
+}
+
+.blurb-container {
+  display: flex; 
+}
+
+.promo-video {
+  border-radius: 5px;
+}
+
+
+.blurb-title {
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+}
+
+.blurb-image {
+  background: transparent;
+}
+
+.step-instructions {
+  display: flex;
+  align-items: center;
+}
+
+.step-text {
+  color: white;
+  width: 200px;
+  margin-left: 20px;
+  font-size: 30px;
+}
+
+.step-text-bottom {
+  margin-top: 30px;
+  color: white;
+  width: 400px;
+}
+
+.step-text-bottom-dark {
+  margin-top: 30px;
+  color: 423F3F;
+  width: 400px;
+}
+
+.step-text-dark {
+  color: #423F3F;
+  width: 200px;
+  margin-left: 20px;
+  font-size: 30px;
+}
+
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 150px;
+  padding-left: 10px;
+  padding-right: 10px;
+  letter-spacing: 2px;
+  line-height: 1.8em;
+  letter-spacing: 3px;
+  color: white;
+  background-image: linear-gradient(135deg, black, rgb(53, 119, 241), rgb(53, 119, 241), rgb(118, 183, 209), rgb(53, 119, 241));
+  height: 75px;
+  font-size: calc(8px + 1vw);
+}
+
+a {
+  color: white;
+  text-decoration: none; /* no underline */
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,165 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div class="skew-background skew-background-up" style="min-height: 140px;">
+      <div class="skew-content-up">
+        saas glue is the universal integration and automation cloud 
+        <br>
+        that lets you run any script on any device anywhere in the world
+      </div>
+    </div>
+
+
+    <div class="blurb-outer-container">
+      <div class="blurb-container">
+        <div class="blurb-spacer"> </div>
+        <div class="blurb-item">
+          saas glue helps you save time and money.  Avoid the same boiler plate work common to so many projects.
+        </div>
+        <div class="blurb-item">
+           <iframe class="promo-video"
+                   allowfullscreen="" 
+                   title="saas glue explainer video" 
+                   src="https://www.youtube.com/embed/7hTv4y3MahE?feature=oembed&amp;start&amp;end&amp;wmode=opaque&amp;loop=0&amp;controls=1&amp;mute=0&amp;rel=0&amp;modestbranding=0"></iframe>
+ 
+        </div>
+      </div>
+    </div>
+
+
+    <div class="skew-background skew-background-up spaced-skew" style="min-height: 350px;">
+      <div class="skew-content-up">
+        <div class="blurb-title">
+          <p>Get started in as little as 5 minutes</p>
+        </div>
+        <div class="blurb-outer-container">
+          <div class="blurb-container">
+            <div class="blurb-item">
+              <img class="blurb-image" src="GetStartedDude.png" width="400px">
+            </div>
+            <div class="blurb-item">
+              <div class="step-instructions">
+                <img src="1YellowCircle.png"> 
+                <div class="step-text">
+                  Sign Up
+                </div>
+              </div>
+              <div class="step-text-bottom">
+                Provide a name and contact information and you’re ready to go.
+              </div>
+            </div>
+            <div class="blurb-spacer"> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="blurb-outer-container">
+      <div class="blurb-container">
+        <div class="blurb-spacer"> </div>
+        <div class="blurb-item">
+          <div class="step-instructions">
+            <img src="2BlueCircle.png"> 
+            <div class="step-text-dark">
+              Install the agent
+            </div>
+          </div>
+          <div class="step-text-bottom-dark">
+            <div>
+              Download the agent and save it to your device.  It will call back to saas glue to establish a connection.
+            </div>
+          </div>
+        </div>
+        <div class="blurb-item">
+          <img class="blurb-image" src="InstallAgentDude.png" width="400px">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="skew-background skew-background-down" style="min-height: 350px;">
+      <div class="skew-content-down">
+        <div class="blurb-outer-container">
+          <div class="blurb-container">
+            <div class="blurb-item">
+              <img class="blurb-image" src="CreateScriptChick.png" width="400px">
+            </div>
+            <div class="blurb-item">
+              <div class="step-instructions">
+                <img src="3RedCircle.png"> 
+                <div class="step-text">
+                  Create a script
+                </div>
+              </div>
+              <div class="step-text-bottom">
+                Upload a script or create one in saas glue.  Use any language you want.
+              </div>
+            </div>
+            <div class="blurb-spacer"> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="blurb-outer-container" style="margin-top: calc(40px + 2vw);">
+      <div class="blurb-container">
+        <div class="blurb-spacer"> </div>
+        <div class="blurb-item">
+          <div class="step-instructions">
+            <img src="4GreenCircle.png"> 
+            <div class="step-text-dark">
+              Run your script
+            </div>
+          </div>
+          <div class="step-text-bottom-dark">
+            <div>
+              The agent will pass the script to the appropriate service for execution and return the results to saas glue.
+            </div>
+          </div>
+        </div>
+        <div class="blurb-item">
+          <img class="blurb-image" src="RunScriptChick.png" width="400px">
+        </div>
+      </div>
+    </div>
+
+
+    <div class="skew-background skew-background-up" style="min-height: 350px;">
+      <div class="skew-content-up">
+        <div class="blurb-outer-container">
+          <div class="blurb-container">
+            <div class="blurb-item">
+              <img class="blurb-image" src="BuildAJobDude.png" width="400px">
+            </div>
+            <div class="blurb-item">
+              <div class="step-instructions">
+                <img src="5OrangeCircle.png"> 
+                <div class="step-text">
+                  Build a job
+                </div>
+              </div>
+              <div class="step-text-bottom">
+                Combine scripts in a job with variables, conditional routing and more.  
+                Set it to run automatically or trigger it through the API.
+              </div>
+            </div>
+            <div class="blurb-spacer"> </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component
+export default class Home extends Vue {  
+}
 </script>
+
+<style scoped>
+
+</style>
